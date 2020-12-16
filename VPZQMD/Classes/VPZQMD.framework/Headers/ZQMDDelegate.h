5f53b5394a817610bf764721a55a258fd342c257
ndk <nidangkun@foxmail.com>
2020-12-16 09:29:06 +0800

    增加跳转页面

--- VPZQMD/Classes/VPZQMD.framework/Headers/ZQMDDelegate.h
+++ VPZQMD/Classes/VPZQMD.framework/Headers/ZQMDDelegate.h
@@ -5,8 +5,9 @@
 #import <UIKit/UIKit.h>
 
 
-
 typedef void (^VPVoidBlock)(void);
+
+typedef void (^VPIndexBlock)(NSInteger index,BOOL result);
 
 typedef void (^ZQMDBlock)(BOOL result,UIViewController *homeVC);
 
@@ -16,7 +17,10 @@ typedef void (^ZQMDBlock)(BOOL result,UIViewController *homeVC);
 
 +(instancetype)shareSingleton;
 
-- (void)launchWithToken:(NSString *)token block:(ZQMDBlock)block;
+/**
+ *destination 0.店铺列表， 1 是创建店铺， 2业务库
+ */
+- (void)launchWithToken:(NSString *)token destination:(NSUInteger)index block:(ZQMDBlock)block vcIndexBlock:(VPIndexBlock)indexBlock;
 
 @property (nonatomic, copy) VPVoidBlock logoutBlock;
 
